e574f557a18960c44550d0000f5652bd520c6769
Evan Hubinger <evhub@ripple.com>
2014-05-06 20:53:26 -0700

    Improves indentation

--- ride.py
+++ ride.py
@@ -169,7 +169,7 @@ Import Commands:
                 elif not instring and x in [")", "]"]:
                     space -= 1
             space += instring
-            if space <= 0 and endswithany(basicformat(last), "=:*+-%/^@~\\|&;<>.,"):
+            if space <= 0 and endswithany(basicformat(last), "=:*+-%/^@~\\|&;<>.,(["):
                 space = 1
             insert = " "*space
             if instring:
