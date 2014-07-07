3b6587268babde232b7cb2fcfb4d617225f3e597
evhub <evanjhub@gmail.com>
2014-07-07 00:34:31 -0700

    Fixes compilation errors

--- rabbit/file.py
+++ rabbit/file.py
@@ -35,7 +35,7 @@ def writefile(openfile, writer):
     """Sets The Contents Of A File."""
     openfile.seek(0)
     openfile.truncate()
-    openfile.write(writer.encode("UTF"))
+    openfile.write(str(writer).encode("UTF"))
 
 def readfile(openfile):
     """Reads The Contents Of A File."""
