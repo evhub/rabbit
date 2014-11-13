ffbd5546862ecc27065dd2ea97be93277fc4bac8
evhub <evanjhub@gmail.com>
2014-11-13 09:07:16 -0800

    Adds more mathematical functions

--- rabbit/carrot/math.py
+++ rabbit/carrot/math.py
@@ -25,9 +25,9 @@ import math
 # CODE AREA: (IMPORTANT: DO NOT MODIFY THIS SECTION!)
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-def succ(x=0.0):
+def succ(x=0):
     """Calculates The Successor Number."""
-    return 1.0+x
+    return 1+x
 
 def gcd(a, b):
     """Calculates The Greatest Common Denominator Of Two Numbers."""
@@ -109,21 +109,21 @@ def primefactor(checknumber):
 
 def roots(a, b, c):
     """Determines The Real Roots Of A Quadratic."""
-    discriminant = b*b-4.0*a*c
+    discriminant = b*b-4*a*c
     if discriminant < 0:
         return None, None
     elif discriminant == 0:
-        return -b/(2.0*a), None
+        return -b/(2*a), None
     else:
-        return (-b+math.sqrt(discriminant))/(2.0*a), (-b-math.sqrt(discriminant))/(2.0*a)
+        return (-b+math.sqrt(discriminant))/(2*a), (-b-math.sqrt(discriminant))/(2*a)
 
 def iroots(a, b, c):
     """Determines The Complex Roots Of A Quadratic."""
-    discriminant = b*b-4.0*a*c
+    discriminant = b*b-4*a*c
     if discriminant == 0:
-        return -b/(2.0*a), None
+        return -b/(2*a), None
     else:
-        return (-b+isqrt(discriminant))/(2.0*a), (-b-isqrt(discriminant))/(2.0*a)
+        return (-b+isqrt(discriminant))/(2*a), (-b-isqrt(discriminant))/(2*a)
 
 def perm(n, k):
     """Determines The Possible Number Of Permutations In n Choose k."""
@@ -149,30 +149,38 @@ def defint(func, start, stop, accuracy=0.0001, strict=False):
     else:
         start += accuracy
         step = (stop-start)*accuracy
-        endpoint = int(1.0/accuracy)
-    out = 0.0
+        endpoint = int(1/accuracy)
+    out = 0
     for x in xrange(0, endpoint):
         point = func(start)
         if x == 0 or x == endpoint:
-            point /= 2.0
+            point /= 2
         out += step*point
         start += step
     return out
 
+def polar_defint(func, start, stop, accuracy=0.0001, strict=False):
+    """Performs A Polar Definite Integral."""
+    return defint(lambda x: func(x)**2, start, stop, accuracy, strict)/2
+
+def length_defint(func, start, stop, accuracy=0.0001, strict=False, scaledown=1.25):
+    """Performs A Length Definite Integral."""
+    return defint(lambda x: isqrt(1+deriv(func, x, accuracy=accuracy, scaledown=scaledown)**2))
+
 def simpson(func, start, stop):
     """Uses Simpson's Rule To Approximate The Definite Integral."""
-    return (func(start) + 4*func((start+stop)/2.0) + func(stop)) * (stop-start)/6.0
+    return (func(start) + 4*func((start+stop)/2) + func(stop)) * (stop-start)/6
 
 def simpson2(func, start, stop):
     """Uses Simpson's 3/8ths Rule To Approximate The Definite Integral."""
-    return (func(start) + 3*func((2*start+stop)/3.0) + 3*func((start+2*stop)/3.0) + func(stop)) * (stop-start)/8.0
+    return (func(start) + 3*func((2*start+stop)/3) + 3*func((start+2*stop)/3) + func(stop)) * (stop-start)/8
 
 def Bnum(n):
     """Calculates The nth Second Bernouli Number."""
     n = int(n)
     a = {}
     for m in xrange(0, n+1):
-        a[m] = 1.0/(m+1.0)
+        a[m] = 1/(m+1)
         for j in reversed(xrange(1, m+1)):
             a[j-1] = j*(a[j-1] - a[j])
     return a[0]
@@ -180,7 +188,7 @@ def Bnum(n):
 def Bpoly(n, x):
     """Calculates The nth Second Bernouli Polynomial."""
     n = int(n)
-    out = 0.0
+    out = 0
     for k in xrange(0, n+1):
         out += comb(n,k)*Bnum(n-k)*x**float(k)
     return out
@@ -193,7 +201,7 @@ def PBpoly(n, x):
 def eulermaclaurin(func, start, stop, p=2, R=True, accuracy=0.0001, scaledown=1.25):
     """Calculates The Error Of The Strict Definite Integral Approximation With An Accuracy Of 1."""
     p = int(p)
-    out = 0.0
+    out = 0
     for k in xrange(2, p+1):
         out -= (deriv(func, start, k-1, accuracy, scaledown) - deriv(func, start, k-1, accuracy, scaledown))*Bnum(k)/math.factorial(k)
     if R:
@@ -205,15 +213,15 @@ def factorial(x):
     if x == -0.5:
         return math.pi**0.5
     elif x > 0 and x-0.5 == int(x-0.5):
-        return x*factorial(x-1.0)
+        return x*factorial(x-1)
     else:
         return math.factorial(x)
 
 def gamma(n, accuracy=0.00000001, stop=None):
     """Implements The Gamma Function Over The Positive Numbers."""
     if stop is None:
-        stop = -n*math.log(accuracy**(1.0/n)/n)/math.log(2.0)
-    return defint(lambda x: x**(n-1.0)*math.e**(-1.0*x), 0.0, stop)
+        stop = -n*math.log(accuracy**(1/n)/n)/math.log(2)
+    return defint(lambda x: x**(n-1)*math.e**(-1*x), 0, stop)
 
 def stirling(n):
     """Implements The Stirling Approximation Of The Factorial."""
@@ -227,20 +235,20 @@ def knuth(a, b, n):
     elif n == 0:
         return a * b
     elif b == 0:
-        return 1.0
+        return 1
     elif n == 1:
         return a ** b
     elif int(a) != a or int(b) != b or a < 0 or b < 0:
         raise ValueError("Knuth up-arrow is only defined over the positive integers")
     else:
-        return knuth(a, knuth(a, b-1.0, n), n-1)
+        return knuth(a, knuth(a, b-1, n), n-1)
 
 def E10(x):
     """Implements 1e(x)."""
-    return 10.0**x
+    return 10**x
 
 def collatz(n, out=None):
-    """Performs The Collatz Function."""
+    """Implements The Collatz Function."""
     if out is None:
         out = []
     if n in out:
@@ -251,3 +259,84 @@ def collatz(n, out=None):
             return collatz(n//2, out)
         else:
             return collatz(n*3+1, out)
+
+def agnesi(x):
+    """Implements The Witch Of Agnesi."""
+    return 1/(1+x**2)
+
+def taylor_terms(f, p=0, n=5, accuracy=0.0001, scaledown=1.25):
+    """Terms Of The Taylor Series."""
+    for i in xrange(0, n):
+        coef = deriv(f, p, i, accuracy, scaledown)/math.factorial(i)
+        yield lambda x: coef*(x-p)**i
+def taylor(f, p=0, n=5, accuracy=0.0001, scaledown=1.25):
+    """Returns The Taylor Expansion For The Function."""
+    terms = taylor_terms(f, p, n, accuracy, scaledown)
+    def expansion(x):
+        """The Series Expansion Of A Function."""
+        return sum((term(x) for term in terms))
+    return expansion
+def maclaurin(f, n=5, accuracy=0.0001, scaledown=1.25):
+    """Returns The Maclaurin Series For The Function."""
+    return taylor(f, 0, n, accuracy, scaledown)
+
+def LHopital(n, top, bot, accuracy=0.0001, scaledown=1.25):
+    """Uses L'Hopital To Solve lim[x->n](top(x)/bottom(x))."""
+    vtop = top(n)
+    vbot = bot(n)
+    i = 1
+    while vtop == 0 and vbot == 0:
+        vtop = deriv(top, n, i, accuracy, scaledown)
+        vbot = deriv(bot, n, i, accuracy, scaledown)
+        i += 1
+    return vtop/vbot
+
+def alternate(i, term):
+    """Alternates A Term By i."""
+    if i%2 == 0:
+        return term
+    else:
+        return -term
+
+def exp_term(x, i):
+    """Term Of The Series For e**x."""
+    return x**i/math.factorial(i)
+def exp_series(x, n):
+    """Expansion Of The Series For e**x."""
+    return sum((exp_term(x, i) for i in xrange(0, n)))
+
+def sin_term(x, i):
+    """Term Of The Series For sin(x)."""
+    n = 2*i+1
+    return alternate(i, exp_term(x, n))
+def sin_series(x, n):
+    """Expansion Of The Series For sin(x)."""
+    return sum((sin_term(x, i) for i in xrange(0, n)))
+
+def cos_term(x, i):
+    """Term Of The Series For cos(x)."""
+    n = 2*i
+    return alternate(i, exp_term(x, n))
+def cos_series(x, n):
+    """Expansion Of The Series For cos(x)."""
+    return sum((cos_term(x, i) for i in xrange(0, n)))
+
+def atan_term(x, i):
+    """Term Of The Series For atan(x)."""
+    n = 2*i+1
+    return alternate(i, x**n/n)
+def atan_series(x, n):
+    """Expansion Of The Series For atan(x)."""
+    return sum((atan_term(x, i) for i in xrange(0, n)))
+
+def polarline(m, b):
+    """Converts A Slope Y-Intercept Line To Polar.""""
+    return lambda theta: b/(math.sin(theta)-m*math.cos(theta))
+
+def sqcos(x):
+    """Calculates cos(x)**2."""
+    return (1+math.cos(2*x))/2
+
+def sqsin(x):
+    """Calculates sin(x)**2."""
+    return (1-math.cos(2*x))/2
