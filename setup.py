58f650332c572744211fac493029dd270334169a
Evan Hubinger <evanjhub@gmail.com>
2018-11-13 18:06:15 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from __future__ import with_statement, print_function, absolute_import, unicode_literals, division
+
+import setuptools
+
+setuptools.setup(
+    name="rabbit",
+    version=0.1,
+    packages=setuptools.find_packages(),
+)
